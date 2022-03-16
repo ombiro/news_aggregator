e48b4814c2695b93ec1f47f4f25e891e0f265038
ombiro <79834034+ombiro@users.noreply.github.com>
2022-03-16 15:17:38 +0300

    commit to master

--- news_extract.py
+++ news_extract.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup as soup
 def get_content_string(url):
     page = requests.get(url)
     page_soup = soup(page.content, 'html.parser')
-    print(page_soup)
 
     containers = page_soup.find_all("script", {"type": "application/ld+json"})
 
@@ -16,6 +15,9 @@ def get_content_string(url):
 
     article_list[0:2] = [''.join(article_list[0:2])]
     content_string = article_list[0]
+
+    article_index = content_string.index("itemListElement")
+    content_string = content_string[article_index + 18:]
     print(content_string)
 
 
